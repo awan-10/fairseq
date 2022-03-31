a2e7788138f2deade441270ca64eef0e7df28882
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2022-04-01 01:38:25 +0500

    add a working running moe script with some docs.

--- run-moe-good.sh
+++ run-moe-good.sh
@@ -1,1 +1,22 @@
-python -m fairseq_cli.interactive  ../downloads/en_moe_lm_15b/ --path ../downloads/en_moe_lm_15b/model.pt --task language_modeling --input input.txt  --is-moe  --distributed-world-size 2 --model-overrides "{'world_size': 2, 'moe_eval_capacity_token_fraction': 0.05}" --bpe gpt2
+world_size=2
+modelname='15b'
+#modelname='52b'
+
+python -m fairseq_cli.interactive  ../downloads/en_moe_lm_${modelname} \
+--path ../downloads/en_moe_lm_${modelname}/model.pt \
+--task language_modeling \
+--input input.txt  \
+--is-moe \
+--distributed-world-size ${world_size} \
+--model-overrides "{'world_size': ${world_size}, 'moe_eval_capacity_token_fraction': 0.05}" \
+--bpe gpt2 \
+--max-len-b 20 \
+--beam 2 
+
+# Documenting some things for help.
+
+# --input input.txt: the input prompt in a local file, e.g. DeepSpeed is
+# --bpe gpt2: needed for dict.txt included in the data path (the first argument)
+# --beam : beam 1 and 2 work for the moe branch -- default is set to 5 and that will fail if we don't specify --beam
+# --max-len-b: set the length of generated output using --max-len-b
+
