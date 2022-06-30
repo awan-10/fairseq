6abb00233a6807dcbcfa543b468fd89d3f5cf282
Ammar Ahmad Awan <ammar.awan@microsoft.com>
2022-07-01 02:49:50 +0500

    run dense model on moe branch.

--- run-dense-single.sh
+++ run-dense-single.sh
@@ -1,2 +1,1 @@
-python -m fairseq_cli.interactive  ../downloads/en_dense_lm_125m/ --path ../downloads/en_dense_lm_125m/model.pt --task language_modeling --input input.txt --bpe gpt2
-#python -m fairseq_cli.interactive  ../fairseq/data-bin/wikitext-103/ --path ../downloads/en_dense_lm_125m/model.pt --task language_modeling --input input.txt
+python -m fairseq_cli.interactive  ../downloads/en_dense_lm_125m/ --path ../downloads/en_dense_lm_125m/model.pt --task language_modeling --input input.txt --bpe gpt2 --max-len-b 20 --beam 2
